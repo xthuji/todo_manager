9add1bed914d362a4ef5c995e547981efc7fb3ea
huji <xthuji@163.com>
2025-10-31 17:54:05 +0800

    test

--- todolist/src/server/server.js
+++ todolist/src/server/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3001;
+const port = 3000;
 
 // 导入路由模块
 const fileRoutes = require('./routes/fileRoutes');
